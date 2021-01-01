179a7c66a8c1c0411aa2fee484d150f6414e2f87
Artur Bien <abien@opera.com>
2021-01-01 20:54:26 +0100

    feat(menu): support anchor element

--- example/src/examples/MenuExample.tsx
+++ example/src/examples/MenuExample.tsx
@@ -1,55 +1,87 @@
 /* eslint-disable no-console */
 import React from 'react';
-import { Menu, Divider, Title } from 'react95-native';
+import { Menu, Divider, Title, Button } from 'react95-native';
 
+import { View } from 'react-native';
 import Container from '../util/Container';
 
 const TextExample = () => {
+  const [verticalMenuOpen, setVerticalMenuOpen] = React.useState(true);
+  const [horizontalMenuOpen, setHorizontalMenuOpen] = React.useState(true);
   return (
     <Container>
       <Container.Section title='Basic usage'>
-        <Menu>
-          <Menu.Item
-            size='lg'
-            onPress={() => console.warn('Item 1')}
-            title='Item 1'
-          />
-          <Menu.Item
-            size='lg'
-            onPress={() => console.warn('Item 2')}
-            title='Item 2'
-          />
-          <Menu.Item
-            size='lg'
-            onPress={() => console.warn('Item 3')}
-            title='Item 3'
-          />
-          <Title>Letters</Title>
-          <Menu.Item size='lg' onPress={() => console.warn('A')} title='A' />
-          <Menu.Item size='lg' onPress={() => console.warn('B')} title='B' />
-          <Menu.Item size='lg' onPress={() => console.warn('C')} title='C' />
-          <Divider size='auto' />
-          <Menu.Item
-            size='lg'
-            disabled
-            onPress={() => console.warn('Disabled Item')}
-            title='Disabled Item'
-          />
-        </Menu>
-        <Menu orientation='horizontal'>
-          <Menu.Item
-            primary
-            onPress={() => console.warn('Item 2')}
-            title='Item 2'
-          />
-          <Menu.Item onPress={() => console.warn('Item 3')} title='Item 3' />
-          <Divider orientation='vertical' variant='raised' size='auto' />
-          <Menu.Item
-            disabled
-            onPress={() => console.warn('Disabled Item')}
-            title='Disabled Item'
-          />
-        </Menu>
+        <View
+          style={{
+            paddingTop: 100,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            width: '100%',
+          }}
+        >
+          <Menu
+            open={verticalMenuOpen}
+            anchor={
+              <Button
+                active={verticalMenuOpen}
+                onPress={() => setVerticalMenuOpen(state => !state)}
+              >
+                Vertical
+              </Button>
+            }
+          >
+            <Menu.Item
+              size='lg'
+              onPress={() => console.warn('Item 1')}
+              title='Item 1'
+            />
+            <Menu.Item
+              size='lg'
+              onPress={() => console.warn('Item 2')}
+              title='Item 2'
+            />
+            <Menu.Item
+              size='lg'
+              onPress={() => console.warn('Item 3')}
+              title='Item 3'
+            />
+            <Title>Letters</Title>
+            <Menu.Item size='lg' onPress={() => console.warn('A')} title='A' />
+            <Menu.Item size='lg' onPress={() => console.warn('B')} title='B' />
+            <Menu.Item size='lg' onPress={() => console.warn('C')} title='C' />
+            <Divider size='auto' />
+            <Menu.Item
+              size='lg'
+              disabled
+              onPress={() => console.warn('Disabled Item')}
+              title='Disabled Item'
+            />
+          </Menu>
+          <Menu
+            orientation='horizontal'
+            open={horizontalMenuOpen}
+            verticalAlignment='above'
+            horizontalAlignment='right'
+            anchor={
+              <Button onPress={() => setHorizontalMenuOpen(state => !state)}>
+                Horizontal
+              </Button>
+            }
+          >
+            <Menu.Item
+              primary
+              onPress={() => console.warn('Item 2')}
+              title='Item 2'
+            />
+            <Menu.Item onPress={() => console.warn('Item 3')} title='Item 3' />
+            <Divider orientation='vertical' variant='raised' size='auto' />
+            <Menu.Item
+              disabled
+              onPress={() => console.warn('Disabled Item')}
+              title='Disabled Item'
+            />
+          </Menu>
+        </View>
       </Container.Section>
     </Container>
   );
--- src/Menu/Menu.tsx
+++ src/Menu/Menu.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleProp, StyleSheet, ViewStyle, View } from 'react-native';
+import type { LayoutChangeEvent } from 'react-native';
 import type { OrientationProp } from '../types';
 
 import { Panel } from '..';
@@ -7,44 +8,84 @@ import { Panel } from '..';
 import MenuItem from './MenuItem';
 
 type Props = React.ComponentPropsWithRef<typeof View> & {
-  // TODO: should we handle 'anchor' and 'overlayAccessibilityLabel' prop?
-  // see react-native-paper
+  anchor?: React.ReactNode;
   children: React.ReactNode;
+  open?: boolean;
   orientation?: OrientationProp;
   style?: StyleProp<ViewStyle>;
+  horizontalAlignment?: 'left' | 'right';
+  verticalAlignment?: 'above' | 'below';
 };
 
 const Menu = ({
+  anchor,
   children,
+  open = false,
   orientation = 'vertical',
   style = {},
+  horizontalAlignment: horizontalAlign = 'left',
+  verticalAlignment: verticalAlign = 'below',
   ...rest
 }: Props) => {
+  const [menuSize, setMenuSize] = React.useState({ width: 0, height: 0 });
+
+  const handleMenuLayout = (e: LayoutChangeEvent) => {
+    const { width, height } = e.nativeEvent.layout;
+    setMenuSize({ width, height });
+  };
+
+  const menuPosition: StyleProp<ViewStyle> = {};
+  if (verticalAlign === 'below') {
+    menuPosition.top = '100%';
+  } else {
+    menuPosition.top = -menuSize.height;
+  }
+  if (horizontalAlign === 'left') {
+    menuPosition.left = 0;
+  } else {
+    menuPosition.right = menuSize.width;
+  }
+
   return (
-    <Panel
-      {...rest}
-      variant='outside'
-      style={[
-        styles.wrapper,
-        {
-          display: 'flex',
-          flexDirection: orientation === 'vertical' ? 'column' : 'row',
-        },
-        style,
-      ]}
-    >
-      {children}
-    </Panel>
+    <View style={styles.wrapper}>
+      {anchor}
+
+      {open && (
+        <View style={[styles.menuWrapper, menuPosition]}>
+          <Panel
+            {...rest}
+            variant='outside'
+            onLayout={handleMenuLayout}
+            style={[
+              styles.menu,
+              {
+                display: 'flex',
+                flexDirection: orientation === 'vertical' ? 'column' : 'row',
+              },
+              style,
+            ]}
+          >
+            {children}
+          </Panel>
+        </View>
+      )}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   wrapper: {
     position: 'relative',
+  },
+  menuWrapper: {
+    position: 'absolute',
+  },
+  menu: {
+    width: 'auto',
+    position: 'absolute',
     height: 'auto',
     flexGrow: 0,
     padding: 6,
-    alignSelf: 'flex-start',
   },
 });
 
