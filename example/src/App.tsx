7edc140d3878ab38d9ced104f78aeb0bf30827a8
Artur Bien <abien@opera.com>
2021-01-14 18:59:05 +0100

    docs(example): add splash screen

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable global-require */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Hourglass, fontNames } from 'react95-native';
+import { fontNames } from 'react95-native';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { LocalThemeProvider } from './util/LocalThemeContext';
 import MainNavigation from './MainNavigation';
@@ -14,19 +15,18 @@ const App = () => {
     [fontNames.bold]: require('./assets/fonts/MS-Sans-Serif-Bold.ttf'),
   });
 
+  useEffect(() => {
+    if (fontLoaded) {
+      SplashScreen.hideAsync();
+      return;
+    }
+
+    SplashScreen.preventAutoHideAsync();
+  }, [fontLoaded]);
+
   if (!fontLoaded) {
-    return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'teal',
-        }}
-      >
-        <Hourglass size={60} />
-      </View>
-    );
+    /* Renders an empty view to avoid errors while the splash screen is visible */
+    return <View />;
   }
 
   return (
